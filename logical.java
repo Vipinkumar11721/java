46dc31dbd7b38375d903d93619b52bf5ce4889d1
Vipin Kumar <vk2386598@gmail.com>
2025-02-14 13:00:40 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,12 @@
+//Write a java program to perform the logical operator ?
+public class logical {
+    public static void main(String[] args) {
+        int a=3;
+        int b=9;
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a = b);
+        System.out.println(a!=b);
+    }
+    
+}
