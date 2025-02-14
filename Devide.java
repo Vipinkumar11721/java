08767bd9528f7e25e38d50319c30d200818ab284
Vipin Kumar <vk2386598@gmail.com>
2025-02-14 12:59:12 +0530

    Add files via upload

---
+++ Devide.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Devide {
+    public static void main(String args[]){
+   Scanner Sc = new Scanner(System.in);
+        System.out.println("Enter number");
+    int a =Sc.nextInt();
+    int b = Sc.nextInt();
+    int devide = a/b;
+    System.out.print(devide);
+    }
+}
